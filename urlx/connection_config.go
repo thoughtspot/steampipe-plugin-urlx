483bf87e1ef61aabaf44f2b76bc406e761c35437
rratshinwork <158099664+rratshinwork@users.noreply.github.com>
2024-02-05 16:01:10 -0800

    Create connection_config.go

---
+++ urlx/connection_config.go
@@ -0,0 +1,26 @@
+package urlx
+
+import (
+	"github.com/turbot/steampipe-plugin-sdk/v5/plugin"
+)
+
+type urlConfig struct {
+	URLs []string `hcl:"urls"`
+	Separator *string `hcl:"separator"`
+	Comment *string `hcl:"comment"`
+	Header *string `hcl:"header"`
+}
+
+func ConfigInstance() interface{} {
+	return &urlConfig{}
+}
+
+// GetConfig :: retrieve and cast connection config from query data
+func GetConfig(connection *plugin.Connection) urlConfig {
+
+	if connection == nil || connection.Config == nil {
+		return urlConfig{}
+	}
+	config, _ := connection.Config.(urlConfig)
+	return config
+}
